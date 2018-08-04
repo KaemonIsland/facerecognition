a9cce5450a3f743affbb082b0867333ccc03295c
KaemonL <kaemonlovendahl14@hotmail.com>
2018-08-03 18:29:35 -0600

    Updated fetch url to application

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
       }
     
       onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://git.heroku.com/blooming-chamber-36125.git/signin', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
